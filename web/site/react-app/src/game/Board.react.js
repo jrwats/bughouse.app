be5048af3fad7a760157b69e589bc1b4fcd0b27b
John Watson <jrwats@gmail.com>
2020-08-06 09:50:15 -0700

    Fix typos

--- web/site/react-app/src/game/Board.react.js
+++ web/site/react-app/src/game/Board.react.js
@@ -1,4 +1,4 @@
-Import React, {useContext, useEffect, useState} from 'react';
+import React, {useContext, useEffect, useState} from 'react';
 import Chessground from 'react-chessground'
 import Holdings from './Holdings.react'
 import PlayerDisplay from './PlayerDisplay.react'
