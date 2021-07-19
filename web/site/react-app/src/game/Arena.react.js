f49553dd6a099dc8e35021b438a9bb225e449693
John Watson <jrwats@gmail.com>
2021-07-19 10:19:47 -0700

    prettier

--- web/site/react-app/src/game/Arena.react.js
+++ web/site/react-app/src/game/Arena.react.js
@@ -1,11 +1,11 @@
-import React, {useContext, useEffect, useState} from 'react';
-import Board from './Board.react';
-import GameStatusSource from './GameStatusSource';
-import {SocketContext} from '../socket/SocketProvider';
-import invariant from 'invariant';
+import React, { useContext, useEffect, useState } from "react";
+import Board from "./Board.react";
+import GameStatusSource from "./GameStatusSource";
+import { SocketContext } from "../socket/SocketProvider";
+import invariant from "invariant";
 import { Redirect } from "@reach/router";
-import { opposite } from 'chessground/util';
-import ScreenLock from './ScreenLock';
+import { opposite } from "chessground/util";
+import ScreenLock from "./ScreenLock";
 
 const Orientation = {
   DEFAULT: 0,
@@ -13,44 +13,49 @@ const Orientation = {
   BLACK: 2, // Black's POV
 };
 
-
-const Arena = ({gamePath, children}) => {
-  const [gameID, orientation] = gamePath.split('~');
+const Arena = ({ gamePath, children }) => {
+  const [gameID, orientation] = gamePath.split("~");
   // const gameID = gamePath;
-  const {handle, socket} = useContext(SocketContext);
+  const { handle, socket } = useContext(SocketContext);
   const gamesSrc = GameStatusSource.get(socket);
 
   const game = gamesSrc.getGame(gameID);
   const boardA = game.getBoardA();
   const boardB = game.getBoardB();
-  const [handleColorA, setHandleColorA] =
-    useState(boardA.getHandleColor(handle));
-  const [handleColorB, setHandleColorB] =
-    useState(boardB != null ? boardB.getHandleColor(handle) : null);
+  const [handleColorA, setHandleColorA] = useState(
+    boardA.getHandleColor(handle)
+  );
+  const [handleColorB, setHandleColorB] = useState(
+    boardB != null ? boardB.getHandleColor(handle) : null
+  );
   useEffect(() => {
     const onboardA = () => {
       const newHC1 = boardA.getHandleColor(handle);
-      console.log(`onboardA ${handle} ${newHC1} ${JSON.stringify(boardA.getBoard())}`);
+      console.log(
+        `onboardA ${handle} ${newHC1} ${JSON.stringify(boardA.getBoard())}`
+      );
       setHandleColorA(newHC1);
     };
     onboardA();
-    boardA.on('init', onboardA);
+    boardA.on("init", onboardA);
     return () => {
-      boardA.off('init', onboardA);
-    }
+      boardA.off("init", onboardA);
+    };
   }, [handle, boardA, gamesSrc]);
   useEffect(() => {
     const onboardB = () => {
       const newHC2 = boardB.getHandleColor(handle);
-      console.log(`onboardB ${handle} ${newHC2} ${JSON.stringify(boardB.getBoard())}`);
-      invariant(boardB != null, 'wtf');
+      console.log(
+        `onboardB ${handle} ${newHC2} ${JSON.stringify(boardB.getBoard())}`
+      );
+      invariant(boardB != null, "wtf");
       console.log(`setHandleColorB(${newHC2})`);
       setHandleColorB(newHC2);
     };
     onboardB();
-    boardB.on('init', onboardB);
+    boardB.on("init", onboardB);
     return () => {
-      boardB.off('init', onboardB);
+      boardB.off("init", onboardB);
     };
   }, [handle, boardB, gamesSrc]);
 
@@ -59,23 +64,34 @@ const Arena = ({gamePath, children}) => {
     console.log(`Arena subscribing ${gameID}`);
     gamesSrc.observe(gameID);
   }, [gamesSrc, gameID]);
-  useEffect(() => { ScreenLock.attemptAcquire(); }, []);
+  useEffect(() => {
+    ScreenLock.attemptAcquire();
+  }, []);
 
   // TODO make this user-controlled/editable (for observers etc)
   if (handleColorB != null) {
-    invariant(handleColorA == null, `Viewer can't be on both boards: ${handleColorA} ${handleColorB}`);
-    let newOrientation = (handleColorB === 'black' ? Orientation.BLACK : 0) | Orientation.FLIPPED;
+    invariant(
+      handleColorA == null,
+      `Viewer can't be on both boards: ${handleColorA} ${handleColorB}`
+    );
+    let newOrientation =
+      (handleColorB === "black" ? Orientation.BLACK : 0) | Orientation.FLIPPED;
     if (orientation != newOrientation) {
       return <Redirect to={`/home/game/${gameID}~${newOrientation}`} />;
     }
   }
 
   // let orientation = null;
-  let viewerOrientation = orientation != null 
-    ? (orientation & Orientation.BLACK ? 'black' : 'white')
-    : (handleColorA || 'white');
-  let orientationA = (orientation & Orientation.FLIPPED) ?
-    opposite(viewerOrientation) : viewerOrientation;
+  let viewerOrientation =
+    orientation != null
+      ? orientation & Orientation.BLACK
+        ? "black"
+        : "white"
+      : handleColorA || "white";
+  let orientationA =
+    orientation & Orientation.FLIPPED
+      ? opposite(viewerOrientation)
+      : viewerOrientation;
 
   const boards = [
     <Board
@@ -89,17 +105,13 @@ const Arena = ({gamePath, children}) => {
       gameID={gameID}
       chessboard={boardB}
       orientation={opposite(orientationA)}
-    />
+    />,
   ];
 
   if (orientation & Orientation.FLIPPED) {
-    boards.reverse()
+    boards.reverse();
   }
-  return (
-    <div style={{width: '100%'}}>
-      {boards}
-    </div>
-  );
+  return <div style={{ width: "100%" }}>{boards}</div>;
 };
 
 export default Arena;
