efd3b2e3a1b7c21d4f3f89c2c42c2d7852d9ee2a
John Watson <jrwats@gmail.com>
2021-08-06 06:48:40 -0700

    canVacat

--- web/site/react-app/src/game/BoardGutter.react.js
+++ web/site/react-app/src/game/BoardGutter.react.js
@@ -2,11 +2,29 @@ import Button from "@material-ui/core/Button";
 import React, { useContext, useEffect, useRef, useState } from "react";
 import HandleDisplay from "./HandleDisplay.react";
 import ClockDisplay from "./ClockDisplay.react";
+import ExitToAppIcon from "@material-ui/icons/ExitToApp";
 import { SocketContext } from "../socket/SocketProvider";
 
+function getHandleDisplay(handle, canVacate, onSit, onVacate) {
+  if (handle == null) {
+    return <Button variant="contained" color="primary" onClick={onSit} >
+      Sit
+    </Button>;
+  }
+  const vacate = canVacate ? 
+    <Button variant="contained" color="secondary" onClick={onVacate}>
+      <ExitToAppIcon style={{ paddingRight: "10px" }} />
+      Vacate
+    </Button> : null ;
+  return <>
+    <HandleDisplay handle={handle} />
+    {vacate}
+  </>;
+}
 
 const BoardGutter = ({ color,  chessboard, forming }) => {
   const {socket} = useContext(SocketContext);
+  const {user} = useContext(AuthContext);
   const playerData = chessboard.getBoard()[color];
   const [handle, setHandle] = useState(playerData?.handle);
 
@@ -26,26 +44,24 @@ const BoardGutter = ({ color,  chessboard, forming }) => {
       chessboard.off("update", onUpdate);
     };
   }, [forming, handle]);
-  let handleDisplay = playerData.handle == null ?
-    <Button
-      style={{ marginTop: "10px" }}
-      variant="contained"
-      color="primary"
-      onClick={() => {
-        console.log(`id: ${chessboard.getGame().getID()}`);
-        socket.sendEvent('sit', { 
-          id: chessboard.getGame().getID(),
-          board: chessboard.getBoardIdx(),
-          color: color === 'white' ? 0 : 1,
-        });
-      }}
-    >
-      Sit
-    </Button>
-    : <HandleDisplay handle={handle} />;
+  console.log(`forming: ${forming}`);
+  const gameData = { 
+    id: chessboard.getGame().getID(),
+    board: chessboard.getBoardIdx(),
+    color: color === 'white' ? 0 : 1,
+  };
+  const onSit = () => {
+    console.log(`id: ${chessboard.getGame().getID()}`);
+    socket.sendEvent('sit', gameData);
+  };
+  const onVacate = () => {
+    console.log(`id: ${chessboard.getGame().getID()}`);
+    socket.sendEvent('vacate', gameData);
+  }
+  const canVacate = forming && handle == user.userN;
   return (
     <div className="playerData">
-      {handleDisplay}
+      {getHandleDisplay(handle, forming, onSit, onVacate)}
       <ClockDisplay color={color} chessboard={chessboard} forming={forming} />
     </div>
   );
