0cf9246944d65a50eae9732c2ddd07ac4fac379f
John Watson <jrwats@gmail.com>
2021-08-12 20:44:35 -0700

    Fixing viewercontext bug with singleton

--- web/site/react-app/src/user/ViewerProvider.js
+++ web/site/react-app/src/user/ViewerProvider.js
@@ -1,39 +1,80 @@
+import { EventEmitter } from "events";
 import React, { createContext, useContext, useEffect, useState } from "react";
 import { AuthContext } from "../auth/AuthProvider";
-import { SocketContext } from "../socket/SocketProvider";
+import SocketProxy from "../socket/SocketProxy";
 
-export const ViewerContext = createContext({
+const DEFAULT_CONTEXT = {
+  deviation: 0,
   fid: null,
+  firebaseUser: null,
   handle: null,
   isGuest: false,
+  rating: 0,
   uid: null,
-  firebaseUser: null,
-});
+}
+export const ViewerContext = createContext(DEFAULT_CONTEXT);
+let _singleton = null;
+
+class ViewerSingleton extends EventEmitter {
+  constructor(user) {
+    super();
+    this._socket = SocketProxy.get();
+    this.onLogin = this._onLogin.bind(this);
+    this.onLogout = this._onLogout.bind(this);
+    this._socket.on('login', this.onLogin)
+    this._socket.on('logout', this.onLogout)
+    this._setViewer(user);
+  }
+
+  _onLogin(data) {
+    if (data.uid !== this._viewer.uid) {
+      console.log(`ViewerSingelton new user?`);
+      this._viewer = {...DEFAULT_CONTEXT, ...data};
+    }
+    this._viewer = {...this._viewer, ...data};
+    this.emit('update', this._viewer);
+  }
+
+  _onLogout(data) {
+    this._setViewer(DEFAULT_CONTEXT);
+  }
+
+  _setViewer(user) {
+    this._viewer = {
+      handle: null,
+      isGuest: false,
+      firebaseUser: user,
+      uid: null,
+      fid: user?.uid,
+      rating: 0,
+      deviation: 0,
+    };
+  }
+
+  getViewer() {
+    return this._viewer;
+  }
+
+  static get(user) {
+    if (_singleton == null) {
+      _singleton = new ViewerSingleton(user);
+    }
+    return _singleton;
+  }
+}
 
 const ViewerProvider = (props) => {
-  const { socket } = useContext(SocketContext);
   const { user } = useContext(AuthContext); // firebase user
-
-  const [state, setState] = useState({
-    handle: null,
-    isGuest: false,
-    firebaseUser: user,
-    uid: null,
-    fid: user?.uid,
-    rating: 0,
-    deviation: 0,
-  });
+  const viewer = ViewerSingleton.get(user);
+  const [state, setState] = useState(viewer.getViewer());
 
   useEffect(() => {
-    const onLogin = (data) => {
-      setState({...state, ...data});
-    };
-
-    socket && socket.on("login", onLogin);  
+    const onUpdate = (data) => setState(data);
+    viewer.on("update", onUpdate);
     return () => {
-      socket && socket.off("login", onLogin);
+      viewer.off("update", onUpdate);
     };
-  }, [socket]);
+  }, [viewer]);
 
   return (
     <ViewerContext.Provider value={state}>
