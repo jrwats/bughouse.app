1cf5938773955e27831e0445b830fb3e0b04aeb8
John Watson <jrwats@gmail.com>
2021-08-25 22:00:54 -0700

    rm worthless script

--- web/line_count.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-find . \( \
-    -path '*node_modules*' -o \
-    -path '*node-telnet-client*' -o \
-    -path '*/build/*' \
-  \) -prune -o -type f -iname '*js' -print0 \
-  | xargs -0 wc -l
