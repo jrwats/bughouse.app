37ad27f910aa2c24b4d9ddbfade856d73bd886f1
John Watson <jrwats@gmail.com>
2021-09-12 18:55:59 -0700

    Enable "encoding" in the b66 binary

--- web/socket/rust/src/bin/b66.rs
+++ web/socket/rust/src/bin/b66.rs
@@ -1,15 +1,24 @@
 use std::env;
 use std::io;
+use uuid::Uuid;
 
 use bughouse_app::b66::B66;
 
 // Right now only useful for converting b66 encoding to a uuid and spitting it out
 fn main() -> Result<(), io::Error> {
     let args: Vec<String> = env::args().collect();
-    let uuid = B66::decode_uuid(&args[1]);
-    if uuid.is_none() {
-        panic!("Invalid uuid");
+    if args[1] == "-e" {
+        let uuid = Uuid::parse_str(&args[2]);
+        if uuid.is_err() {
+            panic!("Couldn't parse `{}` as uuid", args[2]);
+        }
+        println!("{}", B66::encode_uuid(&uuid.unwrap()));
+    } else {
+        let uuid = B66::decode_uuid(&args[1]);
+        if uuid.is_none() {
+            panic!("Invalid uuid");
+        }
+        println!("{}", uuid.unwrap().to_hyphenated());
     }
-    println!("{}", uuid.unwrap().to_hyphenated());
     Ok(())
 }
