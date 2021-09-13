6e7b1dd87f639790121de2f899deea5750d21c71
John Watson <jrwats@gmail.com>
2021-09-12 21:02:56 -0700

    prod scriptz

--- web/ssh-vm.sh
+++ web/ssh-vm.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # usage ssh-vm1 --command='tail /var/log/bughouse.log'
-gcloud beta compute ssh --zone 'us-central1-a' 'vm-20-4' --project 'bughouse-274816' "$@"
+gcloud beta compute ssh --zone 'us-central1-a' 'johnw@vm-20-4' --project 'bughouse-274816' "$@"
