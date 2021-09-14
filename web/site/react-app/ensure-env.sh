0e2a569008f28a1942aa3e9c1867ff46c0c72862
John Watson <jrwats@gmail.com>
2021-09-14 08:54:19 -0700

    Use jrwats/react-chessground as a git URL dependency

--- web/site/react-app/ensure-env.sh
+++ web/site/react-app/ensure-env.sh
@@ -3,14 +3,3 @@ if [[ ! -d .bughouse-secrets ]]; then
   git clone --depth 1 https://github.com/jrwats/bughouse-secrets.git --branch master .bughouse-secrets
 fi
 cp .bughouse-secrets/FirebaseConfig.js src/auth/
-
-if [[ ! -d .react-chessground ]]; then
-  git clone --depth 1 https://github.com/jrwats/react-chessground.git --branch master .react-chessground
-fi
-
-if [[ ! -f .react-chessground/index.js ]]; then
-  pushd .react-chessground > /dev/null
-  trap 'popd > /dev/null' EXIT
-  npm install
-  npm run build
-fi
