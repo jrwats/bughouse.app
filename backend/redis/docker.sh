a4208f0411aaa091223e6124821d7fef832c360f
John Watson <jrwats@gmail.com>
2021-11-16 19:49:54 -0800

    Better Docker script

--- backend/redis/docker.sh
+++ backend/redis/docker.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 
-docker run --name bughouse_redis -p 6379:6379 -d redis redis-server --save 60 1 --loglevel warning
+if docker ps -a -f name=bughouse_redis | grep -q bughouse_redis; then
+  docker start bughouse_redis 
+else
+  docker run --name bughouse_redis -p 6379:6379 -d redis redis-server --save 60 1 --loglevel warning
+fi
+
